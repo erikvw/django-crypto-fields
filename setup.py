899dc4cbd09c09c14f4a3e3654d253f4c7a1e40e
erik van widenfelt <ew2789@gmail.com>
2015-05-17 21:46:24 +0200

    pypi

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ import os
 from setuptools import setup
 from setuptools import find_packages
 
-#with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
-#    README = readme.read()
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-crypto-fields',
-    version='0.1.0',
+    version='0.1.1a',
     author=u'Erik van Widenfelt',
     author_email='ew2789@gmail.com',
     packages=find_packages(),
@@ -19,7 +19,7 @@ setup(
     url='http://github/erikvw/django-crypto-fields',
     license='GPL licence, see LICENCE',
     description='Add encrypted field classes and more to your Django models.',
-    # long_description=README,
+    long_description=README,
     zip_safe=False,
     keywords='django fields encryption security',
     install_requires=[
