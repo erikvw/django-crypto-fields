f105a5169ebeb3fda069f3960de810ab53e37478
erik van widenfelt <ew2789@gmail.com>
2015-05-18 13:05:17 +0200

    add mixin to list encrypted fields

---
+++ django_crypto_fields/mixins/crypto_mixin.py
@@ -0,0 +1,8 @@
+from ..fields import BaseField
+
+
+class CryptoMixin(object):
+
+    @classmethod
+    def encrypted_fields(cls):
+        return [fld.name for fld in cls._meta.fields if isinstance(fld, BaseField)]
--- django_crypto_fields/tests/models.py
+++ django_crypto_fields/tests/models.py
@@ -1,11 +1,14 @@
+from datetime import datetime
+
 from django.db import models
 
 from ..edc.base.models import BaseModel
 
 from ..fields import EncryptedTextField, FirstnameField, IdentityField
+from ..mixins import CryptoMixin
 
 
-class TestModel (BaseModel):
+class TestModel (CryptoMixin, BaseModel):
 
     first_name = FirstnameField(
         verbose_name="First Name")
@@ -17,6 +20,9 @@ class TestModel (BaseModel):
     comment = EncryptedTextField(
         verbose_name="AES",
         max_length=500)
+
+    report_date = models.DateField(
+        default=datetime.today())
 
     objects = models.Manager()
 
