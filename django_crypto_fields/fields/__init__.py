b6c7e6f67cdd743d2a0115bb77fec788dc4faa3a
erikvw <ew2789@gmail.com>
2016-08-03 13:10:52 +0200

    remove unused date field

--- django_crypto_fields/fields/__init__.py
+++ django_crypto_fields/fields/__init__.py
@@ -1,6 +1,6 @@
 from .base_field import BaseField
 from .encrypted_char_field import EncryptedCharField
-from .encrypted_date_field import EncryptedDateField
+# from .encrypted_date_field import EncryptedDateField
 from .encrypted_decimal_field import EncryptedDecimalField
 from .encrypted_integer_field import EncryptedIntegerField
 from .encrypted_text_field import EncryptedTextField
