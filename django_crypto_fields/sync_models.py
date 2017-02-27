db0df649bf06efa48a5a8c8c844cd03c41d0078e
erikvw <ew2789@gmail.com>
2017-02-27 11:53:22 +0200

    remove sync models

--- django_crypto_fields/sync_models.py
+++
@@ -1,9 +0,0 @@
-from edc_sync.site_sync_models import site_sync_models
-from edc_sync.sync_model import SyncModel
-
-
-sync_models = [
-    'django_crypto_fields.Crypt',
-]
-
-site_sync_models.register(sync_models, SyncModel)
