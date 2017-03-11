c7b1a8bd01035b02612c56caf892b7b430dd525c
erikvw <ew2789@gmail.com>
2017-03-11 15:14:49 +0200

    fix edc_base imports

--- django_crypto_fields/migrations/0005_auto_20170106_1849.py
+++ django_crypto_fields/migrations/0005_auto_20170106_1849.py
@@ -3,8 +3,8 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-import edc_base.model.fields.hostname_modification_field
-import edc_base.model.fields.userfield
+import edc_base.model_fields.hostname_modification_field
+import edc_base.model_fields.userfield
 from edc_base.utils import get_utcnow
 
 
@@ -28,7 +28,7 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='crypt',
             name='hostname_modified',
-            field=edc_base.model.fields.hostname_modification_field.HostnameModificationField(blank=True, help_text='System field. (modified on every save)', max_length=50),
+            field=edc_base.model_fields.hostname_modification_field.HostnameModificationField(blank=True, help_text='System field. (modified on every save)', max_length=50),
         ),
         migrations.AlterField(
             model_name='crypt',
@@ -38,11 +38,11 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='crypt',
             name='user_created',
-            field=edc_base.model.fields.userfield.UserField(blank=True, max_length=50, verbose_name='user created'),
+            field=edc_base.model_fields.userfield.UserField(blank=True, max_length=50, verbose_name='user created'),
         ),
         migrations.AlterField(
             model_name='crypt',
             name='user_modified',
-            field=edc_base.model.fields.userfield.UserField(blank=True, max_length=50, verbose_name='user modified'),
+            field=edc_base.model_fields.userfield.UserField(blank=True, max_length=50, verbose_name='user modified'),
         ),
     ]
--- example/models.py
+++ example/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 from django.utils import timezone
 
-from edc_base.model.models import BaseModel
+from edc_base.model_mixins import BaseModel, BaseUuidModel
 
 from django_crypto_fields.fields import EncryptedTextField, FirstnameField, LastnameField, IdentityField
 from django_crypto_fields.mixins import CryptoMixin
-from edc_base.model.models.base_uuid_model import BaseUuidModel
 from django_crypto_fields.crypt_model_mixin import CryptModelMixin
 
 
