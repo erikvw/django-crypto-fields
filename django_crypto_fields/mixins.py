66c494a1402f1822cbdcd96fb1db806747e55944
erikvw <ew2789@gmail.com>
2017-11-09 14:25:02 +0200

    subclass model.Models

--- django_crypto_fields/mixins.py
+++ django_crypto_fields/mixins.py
@@ -1,8 +1,13 @@
+from django.db import models
+
 from .fields import BaseField
 
 
-class CryptoMixin(object):
+class CryptoMixin(models.Model):
 
     @classmethod
     def encrypted_fields(cls):
         return [fld.name for fld in cls._meta.fields if isinstance(fld, BaseField)]
+
+    class Meta:
+        abstract = True
