5f9140f94a02a71bb811b0ef0d42ae1c12d9c9ee
erikvw <ew2789@gmail.com>
2017-12-02 19:34:28 +0200

    drop keyreference model

--- django_crypto_fields/models.py
+++ django_crypto_fields/models.py
@@ -1,43 +1,9 @@
-from django.db import models
 from edc_base.model_mixins import BaseUuidModel
 
 from .model_mixins import CryptModelMixin
-from edc_base.utils import get_utcnow
-
-
-class KeyReferenceError(Exception):
-    pass
 
 
 class Crypt(CryptModelMixin, BaseUuidModel):
 
     class Meta(CryptModelMixin.Meta):
         verbose_name = 'Crypt'
-
-
-class KeyReference(models.Model):
-
-    key_path = models.CharField(
-        max_length=250)
-
-    key_filenames = models.TextField(null=True)
-
-    created = models.DateTimeField(
-        default=get_utcnow, null=True)
-
-    @property
-    def current(self):
-        return self.__class__.objects.all()[0]
-
-    def __str__(self):
-        return self.key_path
-
-    def save(self):
-        try:
-            pk = self.current.pk
-        except IndexError:
-            pass
-        else:
-            if self.id != pk:
-                raise KeyReferenceError(
-                    f'Only one instance of {self._meta.label_lower} may exist.')
