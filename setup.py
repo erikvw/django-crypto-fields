2d0902b26e50aeefedf99d9f1eeb792b7fc3d563
erikvw <ew2789@gmail.com>
2020-08-19 07:04:20 -0500

    update exception message

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
         "Topic :: Security :: Cryptography",
